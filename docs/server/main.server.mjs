644f16ebe480963b1d007863230f1bddddf0859c
Brennan <n3rdtp@gmail.com>
2023-12-21 03:23:30 -0600

    updated site paths to be less predictable and gave an index page for easy navigation

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5GWMP6KD.mjs";import"./chunk-ND4ZQMGX.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-P7T6FJ3F.mjs";import"./chunk-ND4ZQMGX.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};

31bc786a2cca97bb18f436b89c85bc0fca82c77e
Brennan <n3rdtp@gmail.com>
2023-12-22 13:37:44 -0600

    Live adjustments

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-OH7PS7V6.mjs";import"./chunk-ND4ZQMGX.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-FYNXGM4A.mjs";import"./chunk-ND4ZQMGX.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/subcomponents/question-form/question-form.component.ts
+++ src/app/subcomponents/question-form/question-form.component.ts
@@ -16,7 +16,7 @@ export class QuestionFormComponent {
     ['Maurices'],
     ['Culvers'],
     ['Ulta'],
-    ['Crystals'],
+    ['Home'],
     ['Manteno'],
     ['Brickstone']
   ]
